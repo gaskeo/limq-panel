979544e16c97768f585cd4fce1b12f88e1aed1f7
Tikovka72 <tikva@mail.ru>
2020-04-21 23:49:14 +0500

    keygen

---
+++ storage/keygen.py
@@ -0,0 +1,24 @@
+#  _        _   _     _       _                       __  __  ____
+# | |      (_) | |   | |     (_)                     |  \/  |/ __ \
+# | |       _  | |_  | |__    _   _   _   _ __ ___   | \  / | |  | |
+# | |      | | | __| | '_ \  | | | | | | | '_ ` _ \  | |\/| | |  | |
+# | |____  | | | |_  | | | | | | | |_| | | | | | | | | |  | | |__| |
+# |______| |_|  \__| |_| |_| |_|  \__,_| |_| |_| |_| |_|  |_|\___\_\
+
+
+# Token generation stub
+
+import random
+
+LENGTH = 32
+CHARS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+
+
+# Token length is 32 A-Za-z0-9 symbols.
+def generate_key() -> str:
+    return str(random.getrandbits(3)) + "".join(random.choices(CHARS, k=LENGTH - 1))
+
+
+def chan_identifier() -> str:
+    return f"{random.getrandbits(64):x}"
+
