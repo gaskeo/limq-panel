ce3c4e5609b9b88c8b8a4bb3de70eb6a8dabfd33
emmitrin <44097596+emmitrin@users.noreply.github.com>
2022-06-28 03:38:04 +0500

    add manual create table command

--- storage/init.sql
+++ storage/init.sql
@@ -9,4 +9,5 @@ CREATE USER limq_api WITH ENCRYPTED PASSWORD 'acah9bh3fn';
 GRANT ALL PRIVILEGES ON DATABASE limq TO limq_front;
 GRANT ALL PRIVILEGES ON DATABASE limq TO limq_api;
 
-
+-- create buffer table for API
+CREATE TABLE messages (id SERIAL, tag VARCHAR(128), msg_type int, content bytea);
