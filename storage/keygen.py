82dae7c03c3e7d7f7ea20f079ce8c48228fe1681
Tikovka72 <zeytunyan2004@mail.ru>
2022-06-28 23:47:27 +0500

    keygen bug fixed

--- storage/keygen.py
+++ storage/keygen.py
@@ -20,6 +20,7 @@ SALT_LENGTH = 8
 
 CHARS = string.ascii_letters + string.digits
 HEX = string.digits + "abcdef"
+INTS = string.digits
 
 
 def get_random_string(chars: Iterable, k: int = 1) -> str:
@@ -28,7 +29,8 @@ def get_random_string(chars: Iterable, k: int = 1) -> str:
 
 # Token length is 32 A-Za-z0-9 symbols.
 def generate_key() -> str:
-    return get_random_string(CHARS, k=KEY_ID_LENGTH)
+    return get_random_string(INTS, k=1) + \
+           get_random_string(CHARS, k=KEY_ID_LENGTH - 1)
 
 
 def generate_user_id() -> str:
