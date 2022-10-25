93e226f37bdb7d20281fe702b239862180c5e4b2
Tikovka72 <zeytunyan2004@mail.ru>
2022-10-25 15:27:54 +0500

    new init.sql

--- storage/init.sql
+++ storage/init.sql
@@ -23,6 +23,17 @@ CREATE TABLE messages
     content  bytea
 );
 
+CREATE TABLE user_types (
+    type_id integer NOT NULL,
+    name character varying(32),
+    max_channel_count integer NOT NULL,
+    max_message_size integer NOT NULL,
+    bufferization boolean NOT NULL,
+    max_bufferred_message_count integer NOT NULL,
+    buffered_data_persistency integer NOT NULL,
+    end_to_end_data_encryption boolean NOT NULL
+);
+
 INSERT INTO user_types (name,
                         max_channel_count,
                         max_message_size,
